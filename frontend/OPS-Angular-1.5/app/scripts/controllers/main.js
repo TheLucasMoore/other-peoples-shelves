84ed7287d075d59931e0698a4e2d8d188486e1ee
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-10-01 11:30:47 -0500

    Init Angular 1.5

---
+++ frontend/OPS-Angular-1.5/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name opsAngular15App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the opsAngular15App
+ */
+angular.module('opsAngular15App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
